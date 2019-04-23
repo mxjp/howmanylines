7906ee7675a4b10a46699e478829237f6b2dfee1
Max J. Polster <max.polster@id-p.de>
2019-04-23 20:55:34 +0200

    Fix relative gitignore checks.

--- src/gitignore.ts
+++ src/gitignore.ts
@@ -1,5 +1,5 @@
 
-import { join, relative, basename } from 'path';
+import { join, relative, basename, sep } from 'path';
 import { readFile } from 'fs-extra';
 import * as createFilter from 'ignore';
 import * as colors from 'ansi-colors';
@@ -15,7 +15,7 @@ export function gitignore(ctx: Context, names = ['.gitignore']) {
 				pending.push(readFile(ignorefile, 'utf8').then(content => {
 					const filter = (<any> createFilter)().add(content.split('\n'));
 					ctx.excludeCallbacks.push(filename => {
-						if (filename.startsWith(dirname) && filter.ignores(relative(dirname, filename))) {
+						if (filename.startsWith(dirname + sep) && filter.ignores(relative(dirname, filename))) {
 							ctx.detail(`"${colors.red(ctx.formatPath(filename))}" ignored by "${colors.yellow(ctx.formatPath(ignorefile))}"`);
 							return true;
 						}
